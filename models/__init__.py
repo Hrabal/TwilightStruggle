09d87d00a4d8c12b40318fd06fedecbbe181e0a2
Hrabal <federicocerchiai@gmail.com>
2018-04-28 11:20:25 +0200

    added dictable and user models

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
 module_path = os.path.dirname(os.path.abspath(__file__))
-models = [f for f in os.listdir(module_path) if f.endswith('.py') and f != '__init__.py']
+models = [f for f in os.listdir(module_path) if f.endswith('.py') and f not in ('__init__.py', 'dbtools.py')]
 __all__ = models
 print('Imported models: %s' % ', '.join(models) if models else 'No models avaiable in the models directory.')
---
+++ models/user.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from passlib.hash import bcrypt
+
+from app import db
+from models.dbtools import Dictable
+
+
+class User(db.Model, Dictable):
+    user_id = db.Column(db.Integer,
+                        primary_key=True,
+                        autoincrement=True)
+    username = db.Column(db.String(64),
+                         index=True,
+                         unique=True,
+                         nullable=False)
+    password_hash = db.Column(db.String(300),
+                              nullable=False)
+    email = db.Column(db.String(300))
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password_hash = bcrypt.encrypt(password)
+        self.email = email
+
+    def validate_password(self, password):
+        return bcrypt.verify(password, self.password_hash)
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.user_id)
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
