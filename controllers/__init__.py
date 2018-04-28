5a02e2a234a3caae5a21af021698a00def672cf8
Hrabal <federicocerchiai@gmail.com>
2018-04-28 11:13:07 +0200

    controllers dir

---
+++ controllers/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import os
+import importlib
+
+module_path = os.path.dirname(os.path.abspath(__file__))
+controllers = [f for f in os.listdir(module_path) if f.endswith('.py') and f != '__init__.py']
+__all__ = controllers
+for view in controllers:
+    importlib.import_module('controllers.%s' % view[:-3])
+
+print('Imported controllers: %s' % ', '.join(controllers) if controllers else 'No controllers avaiable in the controllers directory.')
