cf9aa5fdc837621b132d603b9acd9cfc7c7ed2d9
Hrabal <federicocerchiai@gmail.com>
2018-04-29 18:57:48 +0200

    Login page

--- views/user.py
+++ views/user.py
@@ -9,8 +9,7 @@ class LoginPage(base.BasePage):
     def init(self):
         self.form = tags.Form(action="/login",
                               method='post',
-                              klass='form-signin')(
-            title=tags.H2(klass='form-signin-heading')('Login'),
+                              klass='omb_loginForm')(
             username=tags.Input('required', 'autofocus',
                                 typ="text",
                                 name="username",
@@ -31,7 +30,51 @@ class LoginPage(base.BasePage):
                                 name='referrer'),
         )
         self.content(
-            form=self.form
+            tags.Div(klass='omb_login')(
+                tags.H3(klass='omb_authTitle')(
+                    'Login or ',
+                    tags.A(href='/signup')('Sign up')
+                ),
+                tags.Div(klass='row omb_row-sm-offset-3 omb_socialButtons')(
+                    tags.Div(klass='col-xs-4 col-sm-3')(
+                        tags.A(href='#', klass='btn btn-lg btn-block omb_btn-facebook')(
+                            tags.I(klass='fas fa-facebook visible-xs'),
+                            tags.Span(klass='hidden-xs')('Facebook')
+                        )
+                    ),
+                    tags.Div(klass='col-xs-4 col-sm-3')(
+                        tags.A(href='#', klass='btn btn-lg btn-block omb_btn-google')(
+                            tags.I(klass='fas fa-google-plus visible-xs'),
+                            tags.Span(klass='hidden-xs')('Google')
+                        )
+                    )
+                ),
+                tags.Div(klass='row omb_row-sm-offset-3 omb_loginOr')(
+                    tags.Div(klass='col-xs-12 col-sm-6')(
+                        tags.Hr(klass='omb_hrOr'),
+                        tags.Span(klass='omb_spanOr')('or')
+                    )
+
+                ),
+                tags.Div(klass='row omb_row-sm-offset-3')(
+                    tags.Div(klass='col-xs-12 col-sm-6')(
+                        self.form
+                    )
+                ),
+                tags.Div(klass='row omb_row-sm-offset-3 omb_loginOr')(
+                    tags.Div(klass='col-xs-12 col-sm-3')(
+                        tags.Label(klass='checkbox')(
+                            tags.Input(typ='checkbox', value='remember-me'),
+                            'Remember Me'
+                        )
+                    ),
+                    tags.Div(klass='col-xs-12 col-sm-3')(
+                        tags.P(klass='omb_forgotPwd')(
+                            tags.A(href='#')('Forgot Password')
+                        )
+                    )
+                )
+            )
         )
 
     def wrong(self, what):
